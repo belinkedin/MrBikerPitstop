6513050149767c86e5e2e6544cb729ed61b05c47
belinkedin <belinkedindindigul@gmail.com>
2026-01-21 19:06:14 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/MrBikerPitstop">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
